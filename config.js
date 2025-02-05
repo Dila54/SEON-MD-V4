dc8eefdc873c6d645596e0f4b04b784b12694756
Dila54 <Sudarshanayasidu1@gmail.com>
2025-02-05 19:49:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || '', //Put Session-id Here
-GITHUB_USERNAME: process.env.GITHUB_USERNAME || '', //Put Your Gitub Username
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || '', //Put Your Github Auth Token
-REPO_NAME: process.env.REPO_NAME || 'SEON-MD-DATABASE' //Bot Database Repo Name
+SESSION_ID: process.env.SESSION_ID || 'SEON-MD=KdolHQgb#3akmZNgdgoRMcWoqkY2E5GGE-ES6C2-YyEmfMuL0eU4', //Put Session-id Here
+GITHUB_USERNAME: process.env.GITHUB_USERNAME || 'Dila54', //Put Your Gitub Username
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || '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', //Put Your Github Auth Token
+REPO_NAME: process.env.REPO_NAME || 'Seon md' //Bot Database Repo Name
 };
